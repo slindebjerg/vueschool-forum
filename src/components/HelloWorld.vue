8103d23ffabd73c76b48b3e7ebfaaf3f63921430
slindebjerg <proj.alph@gmail.com>
2018-09-16 19:11:22 +0200

    Changing accounts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,7 +25,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'I will just change this to make a new commit.'
     }
   }
 }
