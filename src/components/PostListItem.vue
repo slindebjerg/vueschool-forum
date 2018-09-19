42d531a90a56a659fe84ef1fe50fc82b785a1a60
slindebjerg <proj.alph@gmail.com>
2018-09-19 16:36:33 +0200

    Saving progress

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -15,14 +15,16 @@
       </div>
     </div>
 
-    <div class="post-date text-faded">
-      {{post.publishedAt}}
+    <div class="post-date text-faded"
+    :title="post.publishedAt | humanFriendlyDate">
+      {{post.publishedAt | diffForHumans}}
     </div>
     </div>
 </template>
 
 <script>
   import sourceData from '@/data'
+  import moment from 'moment'
 
   export default{
     props: {
@@ -39,6 +41,16 @@
 
       postCount () {
         return Object.keys(this.user.posts).length
+      }
+    },
+
+    filters: {
+      humanFriendlyDate (date) {
+        return moment.unix(date).format('Do MMMM YYYY, hh:mm')
+      },
+
+      diffForHumans (date) {
+        return moment.unix(date).fromNow()
       }
     }
   }
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,6 +2,22 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts"/>
+    <form v-on:submit.prevent="addPost">
+      <div class="form-group">
+        <textarea
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        class="form-input"
+        :value="newPostText"
+        @input="newPostText = $event.target.value"
+        ></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -9,25 +25,47 @@
   import sourceData from '@/data'
   import PostList from '@/components/PostList'
   export default {
+
     components: {
       PostList
     },
+
     props: {
       id: {
         required: true,
         type: String
       }
     },
+
     data () {
       return {
-        thread: sourceData.threads[this.id]
+        thread: sourceData.threads[this.id],
+        newPostText: ''
       }
     },
+
     computed: {
       posts () {
         const postIds = Object.values(this.thread.posts)
         return Object.values(sourceData.posts)
           .filter(post => postIds.includes(post['.key']))
+      }
+    },
+
+    methods: {
+      addPost () {
+        const postId = 'greatPost' + Math.random()
+        const post = {
+          text: this.newPostText,
+          publishedAt: Math.floor(Date.now() / 1000),
+          threadId: this.id,
+          userId: 'Miej9zSGMRZKDvMXzfxjVOyv3RF3',
+          '.key': postId
+        }
+        this.$set(sourceData.posts, postId, post)
+        this.$set(this.thread.posts, postId, postId)
+        this.$set(sourceData.users[post.userId].posts, postId, postId)
+        this.newPostText = ''
       }
     }
   }
